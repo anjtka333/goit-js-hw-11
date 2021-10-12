f52981be04b532435dedfa6d246ffb97be4bc3c9
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-10-12 18:18:03 +0300

    done

--- src/index.js
+++ src/index.js
@@ -4,28 +4,43 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { InfiniteScroll } from '../node_modules/infinite-scroll/js/index';
+
 const lightbox = new SimpleLightbox('.gallery a', {
   /* options */
 });
+const axios = require('axios');
 let num = 1;
 
 const gallery = document.querySelector('.gallery');
 const searchForm = document.querySelector('.search-form');
 const loadMoreBtn = document.querySelector('.load-more');
 loadMoreBtn.style.display = 'none';
+// let elem = document.querySelector('.container');
+// let infScroll = new InfiniteScroll(elem, {
+//   // options
+//   path: '.pagination__next',
+//   append: '.post',
+//   history: false,
+// });
 const getSearchedResults = async e => {
   const requestName = searchForm.elements['searchQuery'].value;
   const result = await fetchPictures(requestName, num);
   if (result.hits.length === 0) {
     loadMoreBtn.style.display = 'none';
     return Notify.failure("We're sorry, but you've reached the end of search results.");
+  } else {
+    loadMoreBtn.style.display = 'inline-block';
   }
   if (result.total === 0) {
     loadMoreBtn.style.display = 'none';
     return console.log('Sorry, there are no images matching your search query. Please try again.');
   }
+  if (num >= 2) {
+    Notify.success(`Hooray! We found ${result.totalHits} images.`);
+  }
   console.log(result);
-
   renderPictureListItems(result);
 };
 
@@ -33,17 +48,36 @@ searchForm.addEventListener('submit', e => {
   e.preventDefault();
   gallery.innerHTML = '';
   num = 1;
-  loadMoreBtn.style.display = 'inline-block';
   getSearchedResults();
 });
+
+const slowScroll = () => {
+  const { height: cardHeight } = document.querySelector('.gallery');
+  console.log(document.querySelector('.gallery').offsetHeight);
+  window.scrollBy({
+    top: document.querySelector('.gallery').offsetHeight,
+    behavior: 'smooth',
+  });
+};
 const renderPictureListItems = async ({ hits }) => {
   let render = '';
+  let sweecher = false;
+  let counter = 0;
   for (let i = 0; i < hits.length; i++) {
-    render += `<div class="photo-card">
-   <a href="${hits[i].webformatURL}"> <img src="${hits[i].largeImageURL}" alt="${hits[i].tags}" loading="lazy" /><a>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes <span>${hits[i].likes}</span></b>
+    if (i % 4 === 0 && sweecher === true) {
+      render += `</div>`;
+      sweecher = false;
+    }
+    if (i % 4 === 0 && sweecher === false) {
+      render += `<div class="row ">`;
+      sweecher = true;
+    }
+    render += `<div class="photo-card card col-3 m-1 shadow-sm p-3 mb-5 bg-white rounded" style="width: 16rem;">
+   <a class='card-img-top' href="${hits[i].largeImageURL}"> <img style="height: 200px;" 
+   class='img-fluid img-thumbnail' src="${hits[i].webformatURL}" alt="${hits[i].tags}" loading="lazy" /></a>
+    <div class="info card-body">
+      <p class="info-item card-title">
+        <b class= "card-text">Likes <span>${hits[i].likes}</span></b>
       </p>
       <p class="info-item">
         <b>Views <span>${hits[i].views}</span></b>
@@ -58,9 +92,15 @@ const renderPictureListItems = async ({ hits }) => {
   </div>`;
   }
   gallery.insertAdjacentHTML('beforeend', render);
+  // document.querySelectorAll('.img-thumbnail').style.height = "200px";
+  lightbox.refresh();
+  slowScroll();
 };
 loadMoreBtn.addEventListener('click', async e => {
   ++num;
   await getSearchedResults();
   console.log(num);
 });
+// document.addEventListener('scroll', () => {
+
+// });
--- src/js/fetchPictures.js
+++ src/js/fetchPictures.js
@@ -1,11 +1,13 @@
 import { num } from '../index';
+const axios = require('axios');
 export const fetchPictures = async (requestName, num) => {
   num = num;
-  const response = await fetch(
+  const response = await axios.get(
     `https://pixabay.com/api/?key=23779410-abe809331958b49ace969e642&q=${requestName}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${num}`,
   );
+  console.log(response);
   if (response.status >= 200 && response.status < 300) {
-    const users = await response.json();
+    const users = await response.data;
     return users;
   } else return Promise.reject('Requst error');
 };
